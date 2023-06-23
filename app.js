12096880e6e0099cf075b2457999edb0551ec5cb
Souradip <dgsouro77@gmail.com>
2023-06-23 20:54:33 +0530

    Added all files

---
+++ app.js
@@ -0,0 +1,24 @@
+const generateMemebutn=document.querySelector(
+    ".meme-generator .generate-meme-butn");
+
+const MemeImage=document.querySelector(".meme-generator img");
+const MemeTitle=document.querySelector(".meme-generator .meme-title");
+const MemeAuthor=document.querySelector(".meme-generator .meme-author");
+
+const updateDetails = (url, title, author) => {
+    MemeImage.setAttribute("src", url);
+    MemeTitle.innerHTML=title;
+    MemeAuthor.innerHTML=author;
+};
+
+const  generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+        .then((response) => response.json())
+        .then((data) => {
+            updateDetails(data.url, data.title, data.author);
+        });
+};
+
+generateMemebutn.addEventListener("click", generateMeme);
+
+generateMeme();
